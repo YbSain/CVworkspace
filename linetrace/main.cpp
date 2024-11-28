ca732eb9ce4dd12624b23dc94cc75ed3d371c3c8
YbSain <108385276+YbSain@users.noreply.github.com>
2024-11-28 14:48:32 +0900

    Update main.cpp

--- linetrace/main.cpp
+++ linetrace/main.cpp
@@ -1,5 +1,7 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <signal.h>
+#include "dxl.hpp"
 
 using namespace cv;
 using namespace std;
@@ -19,8 +21,19 @@ int main(void) {
     Rect targetBoundingBox;
     Point2d targetCenter;
     bool isTarget = false;
+    Dxl mx;
+    struct timeval start, end1;
+    double diff1;
+    int vel1 = 0, vel2 = 0;
+    int goal1 = 0, goal2 = 0;
+    signal(SIGINT, ctrlc);
+    if (!mx.open()) {
+        cout << "dxl open error" << endl;
+        return -1;
+    }
 
     while (true) {
+        gettimeofday(&start, NULL);
         cap >> frame;
         if (frame.empty()) {
             break;
@@ -83,13 +96,26 @@ int main(void) {
             }
 
         }
-
+        // generate accel and decel motion
+        if (goal1 > vel1) vel1 += 5;
+        else if (goal1 < vel1) vel1 -= 5;
+        else vel1 = goal1;
+        if (goal2 > vel2) vel2 += 5;
+        else if (goal2 < vel2) vel2 -= 5;
+        else vel2 = goal2;
+        if (!mx.setVelocity(vel1, vel2)) { cout << "setVelocity error" << endl; return -1; }
+        if (ctrl_c_pressed) break;
+        usleep(20 * 1000);
+        gettimeofday(&end1, NULL);
+        diff1 = end1.tv_sec + end1.tv_usec / 1000000.0 - start.tv_sec - start.tv_usec / 1000000.0;
+        cout << "vel1:" << vel1 << ',' << "vel2:" << vel2 << ",time:" << diff1 << endl;
 
 
         imshow("morph", displaymorph);
         imshow("cap", frame);
         if (waitKey(30) == 27) break;
     }
+    mx.close();
     cap.release();
     destroyAllWindows();
     return 0;
