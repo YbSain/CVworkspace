a2bd41230c4216820af514fa1893b21b4fe1d5af
YbSain <108385276+YbSain@users.noreply.github.com>
2024-11-28 14:19:58 +0900

    Update main.cpp

--- linetrace/main.cpp
+++ linetrace/main.cpp
@@ -11,10 +11,15 @@ void ctrlc(int) {
 int main(void) {
     TickMeter tm;
     //원본
-    VideoCapture cap("5_lt_cw_100rpm_out.mp4");
+    VideoCapture cap("7_lt_ccw_100rpm_in.mp4");
+
     if (!cap.isOpened()) { cerr << "Video Open Failed!" << endl; return -1; }
 
     Mat frame, gray, thresh, labels, stats, centroids;
+    Rect targetBoundingBox;
+    Point2d targetCenter;
+    bool isTarget = false;
+
     while (true) {
         cap >> frame;
         if (frame.empty()) {
@@ -40,32 +45,48 @@ int main(void) {
 
         Scalar meanValueAdjust = mean(fixbrightnessImg);
         double adjustedBrightness = meanValueAdjust[0];
-        cout << "Adjust Average Brightness:" << adjustedBrightness << endl;
         //이진화
         threshold(fixbrightnessImg, thresh, 165, 255, THRESH_BINARY);
         //라인 검출
-        Mat connected = Roiframe.clone();
+        
         //모폴로지 / 노이즈
-        Mat mopho;
-        morphologyEx(thresh, mopho, MORPH_OPEN, Mat(), Point(-1, -1), 3);
-        int cnt = connectedComponentsWithStats(mopho, labels, stats, centroids);
-
+        Mat morph;
+        morphologyEx(thresh, morph, MORPH_OPEN, Mat(), Point(-1, -1));
+        erode(morph, morph, Mat(), Point(-1, -1), 2);
+        Mat displaymorph;
+        cvtColor(gray, displaymorph, COLOR_GRAY2BGR);
+        static Point2d firstCenter(morph.cols / 2, morph.rows / 2);
+        int cnt = connectedComponentsWithStats(morph, labels, stats, centroids);
         for (int i = 1; i < cnt; i++) {
             int* p = stats.ptr<int>(i);
             double* c = centroids.ptr<double>(i);
             if (p[4] < 20) continue;
+            Rect currentBoundingBox(p[0], p[1], p[2], p[3]);
+            Point2d currentCenter(c[0], c[1]);
+            int error = morph.cols / 2 - (int)c[0];
+            
+            double xdistance = targetCenter.x - c[0];
+            if (!isTarget) {
+                targetCenter = firstCenter;
+                isTarget = true;
+            }
+            else {
+                if ((xdistance <= 80 && xdistance >= -80)) {
+                    rectangle(displaymorph, Rect(p[0], p[1], p[2], p[3]), Scalar(0, 255, 255), 2);
+                    circle(displaymorph, targetCenter, 3, Scalar(255, 255, 0), 2);
+                    circle(displaymorph, currentCenter, 5, Scalar(0, 255, 0), FILLED);
+                    targetBoundingBox = currentBoundingBox;
+                    targetCenter = currentCenter;
+                    cout << "error" << error << endl;
+                }
 
-            circle(connected, Point((int)c[0], (int)c[1]), 5, Scalar(0, 255, 0), FILLED);
-            rectangle(connected, Rect(p[0], p[1], p[2], p[3]), Scalar(0, 255, 255), 2);
+            }
+            
         }
         
-        imshow("mopho", mopho);
 
 
-        imshow("connected", connected);
-        imshow("thresh", thresh);
-        imshow("Adjustimg", fixbrightnessImg);
-        imshow("gray", gray);
+        imshow("morph", displaymorph);
         imshow("cap", frame);
         if (waitKey(30) == 27) break;
     }
